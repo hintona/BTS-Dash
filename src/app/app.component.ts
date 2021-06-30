bbcca10755f3164f48aa8e6bf9b1599091b2d07b
synfy <alexandra.hinton18@ncf.edu>
2021-06-30 16:18:17 -0500

    Spotify player made collapisble, collapsed by default

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'bts-dash';
+
+  expanded: boolean;
+  
+  constructor(){
+    this.expanded = false;
+  }
+
+  toggleClosed() {
+    const counter = 0;
+    if(this.expanded){
+      this.expanded = false;
+    }
+    else if(!this.expanded){
+      this.expanded = true;
+    }
+    console.log(this.expanded);
+    console.log(counter);
+  }
 }
