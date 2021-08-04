a741c400ead736e26916586d2d5375dff3f229a3
synfy <alexandra.hinton18@ncf.edu>
2021-08-04 14:20:29 -0500

    Only one section open at a time, visual indicator of which one is open

--- src/app/data-dropdown/data-dropdown.component.ts
+++ src/app/data-dropdown/data-dropdown.component.ts
@@ -1,15 +1,55 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-data-dropdown',
   templateUrl: './data-dropdown.component.html',
   styleUrls: ['./data-dropdown.component.css']
 })
-export class DataDropdownComponent implements OnInit {
+export class DataDropdownComponent{
 
-  constructor() { }
+  likeshares:boolean;
+  followers:boolean;
+  impressions:boolean;
+  engagement:boolean;
 
-  ngOnInit(): void {
-  }
+  constructor() {
+    this.likeshares = false;
+    this.followers = false;
+    this.impressions = false;
+    this.engagement = false;
+   }
+
+   toggle(changed:string){
+     if(changed == 'likeshares'){
+       if(this.likeshares == true) this.likeshares = false;
+       else this.likeshares = true;
+       this.followers = false;
+       this.impressions = false;
+       this.engagement = false;
+     }
+     if(changed == 'followers'){
+      if(this.followers == true) this.followers = false;
+      else this.followers = true;
+      this.likeshares = false;
+      this.impressions = false;
+      this.engagement = false;
+    }
+    if(changed == 'impressions'){
+      if(this.impressions == true) this.impressions = false;
+      else this.impressions = true;
+      this.followers = false;
+      this.likeshares = false;
+      this.engagement = false;
+    }
+    if(changed == 'engagement'){
+      if(this.engagement == true) this.engagement = false;
+      else this.engagement = true;
+      this.followers = false;
+      this.impressions = false;
+      this.likeshares = false;
+    }
+   }
+  
+  
 
 }
